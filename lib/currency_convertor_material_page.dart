77a28acc9fbef4a836c5654fec64d8b179af0dca
Aarush-Parashar <aarushparashar2004@gmail.com>
2024-05-05 16:43:29 +0530

    git commit -m'improved the app'

--- lib/currency_convertor_material_page.dart
+++ lib/currency_convertor_material_page.dart
@@ -6,29 +6,31 @@ class CurrencyConvertorMaterialPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-        backgroundColor: Colors.blueGrey,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                '0',
-                style: TextStyle(
-                    fontSize: 55,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 255, 255, 255)),
-              ),
-              TextField(
-                style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
-                decoration: InputDecoration(
-                  label: Text(
-                    "Please enter an amount in INR:",
-                    style: TextStyle(color: Colors.white),
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '0',
+              style: TextStyle(
+                  fontSize: 55,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 255, 255, 255)),
+            ),
+            TextField(
+              style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+              decoration: InputDecoration(
+                  hintText: "Please enter the amount in USD",
+                  hintStyle: TextStyle(
+                    color: Colors.white60,
                   ),
-                ),
-              ),
-            ],
-          ),
-        ));
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  prefixIconColor: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
