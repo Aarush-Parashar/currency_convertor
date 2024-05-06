a4f7de505da524da9948f2e4b968468536c1e863
Aarush-Parashar <aarushparashar2004@gmail.com>
2024-05-06 22:16:16 +0530

    added a button

--- lib/currency_convertor_material_page.dart
+++ lib/currency_convertor_material_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CurrencyConvertorMaterialPage extends StatelessWidget {
@@ -16,6 +17,17 @@ class CurrencyConvertorMaterialPage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: const Text(
+          'Currency Convertor',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.blueGrey,
+        elevation: 0,
+        centerTitle: true,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -27,7 +39,7 @@ class CurrencyConvertorMaterialPage extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                   color: Color.fromARGB(255, 255, 255, 255)),
             ),
-            Container(
+            Padding(
               padding: const EdgeInsets.all(10.0),
               child: TextField(
                 style: const TextStyle(color: Colors.black),
@@ -46,6 +58,25 @@ class CurrencyConvertorMaterialPage extends StatelessWidget {
                 keyboardType: const TextInputType.numberWithOptions(
                   decimal: true,
                 ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+              child: TextButton(
+                onPressed: () {
+                  if (kDebugMode) {
+                    print('Button Clicked');
+                  }
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: const Text("Convert"),
               ),
             )
           ],
